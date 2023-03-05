b0062cf72da0dcc8ffb76f722100f17fbd493162
Jay Jo <sulyman153@gmail.com>
2023-03-05 05:48:55 -0800

    new commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * a C program that prints a text with puts followed by a new line
+ * main - Entry point of the program
+ * Return: Always returns (0)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
