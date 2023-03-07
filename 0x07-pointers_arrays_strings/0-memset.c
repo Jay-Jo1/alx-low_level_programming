3bd8279f0643290fc1d07b292d99a7cfda923786
Jay Jo <sulyman153@gmail.com>
2023-03-06 18:45:38 -0800

    task 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
-  * _memset - fills the memory with a constant bite
-  * @n: The specified number of bytes
-  * @s: The pointer of type char
-  * @b: The constant byte
-  * Return: Returns a pointer to the memory area 's'
-  */
+ * _memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (Success)
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
 		s[i] = b;
-	}
-
 	return (s);
 } 
